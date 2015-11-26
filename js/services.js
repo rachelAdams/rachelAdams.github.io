c7fb9ec3748ae23914d3a9f2898fddd2ef8457bf
rachelAdams <rachel.adams1012@gmail.com>
2015-11-26 11:03:45 -0800

    playing with ajax call

--- js/services.js
+++ js/services.js
@@ -6,7 +6,13 @@ angular.module('rachelApp.services', []).
     githubApi.getRepos = function() {
       return $http({
         method: 'GET', 
-        url: 'https://api.github.com/users/racheladams/repos'
+        url: 'https://api.github.com/users/racheladams/repos',
+        success: function (data, status, error) {
+          githubApi = data;
+    },
+    error: function (data, status, error) {
+      console.log('error', data, status, error);
+    }
       });
     }
 
