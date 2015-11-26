5be77e89c81abf9ce70d309fcc0a15577db9490e
rachelAdams <rachel.adams1012@gmail.com>
2015-11-26 11:07:38 -0800

    more angular

--- js/services.js
+++ js/services.js
@@ -1,20 +1,19 @@
 angular.module('rachelApp.services', []).
   factory('githubApiService', function($http) {
 
-    var githubApi = {};
+    var githubRepos = {};
 
     githubApi.getRepos = function() {
-      return $http({
-        method: 'GET', 
+      $http.get({
         url: 'https://api.github.com/users/racheladams/repos',
         success: function (data, status, error) {
-          githubApi = data;
-    },
-    error: function (data, status, error) {
-      console.log('error', data, status, error);
-    }
+          githubRepos = data;
+        },
+        error: function (data, status, error) {
+          console.log('error', data, status, error);
+        }
       });
     }
 
-    return githubApi;
+    return githubRepos;
   });
