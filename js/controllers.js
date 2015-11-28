b6b5fd3bbf7f6f5b79015e45f9305341ccce2a42
rachelAdams <rachel.adams1012@gmail.com>
2015-11-28 15:42:39 -0800

    added local storage

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,19 @@
 angular.module('rachelApp.controllers', [])
 .controller('rachelCtrl', function ($scope, githubApiService) {
     $scope.name = "Rachel Adams";
+	$scope.localStorageVal = localStorage.getItem("localVal");
+    githubApiService.getRepos().success(function (response) {
+    //Dig into the responde to get the relevant data
+    $scope.drivers = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    $scope.addDriver = function(){
+         	
+    }
 
-        githubApiService.getRepos().success(function (response) {
-        //Dig into the responde to get the relevant data
-         $scope.repos = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+	$scope.saveToLocalStorage = function(){
+		if(typeof(Storage) !== "undefined") {
+    		localStorage.setItem("localVal", $scope.localStorageVal); // Code for localStorage/sessionStorage.
+		} 
+	}
+        
     });
 });
