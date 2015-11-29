14822e285c7313f988656667f21bf92f373c7204
rachelAdams <rachel.adams1012@gmail.com>
2015-11-28 16:39:12 -0800

    bug fix hopefully

--- js/app.js
+++ js/app.js
@@ -1,11 +1,7 @@
 
-angular.module('rachelApp', [
-  'rachelApp.controllers',
-  'rachelApp.services'
-]).directive('mySharedScope', function () {
+angular.module('rachelApp', [ 'rachelApp.controllers', 'rachelApp.services'])
+.directive('mySharedScope', function () {
     return {
-        template: 'Name: <span ng-bind="driver.Driver.givenName"></span> 
-        <span ng-bind="driver.Driver.familyName"></span><br />
-        Wikipedia: <span ng-bind="driver.Driver.url"></span>'
+        template: "Name: <span ng-bind="driver.Driver.givenName"></span> <span ng-bind="driver.Driver.familyName"></span><br /> Wikipedia: <span ng-bind="driver.Driver.url"></span>"
     };
-});;
+});
