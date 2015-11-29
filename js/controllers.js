b1f62c202cc3d6c99fc9c675d812747f4f7fdaaa
rachelAdams <rachel.adams1012@gmail.com>
2015-11-28 16:08:35 -0800

    local storage and styling

--- js/controllers.js
+++ js/controllers.js
@@ -1,19 +1,23 @@
 angular.module('rachelApp.controllers', [])
 .controller('rachelCtrl', function ($scope, githubApiService) {
     $scope.name = "Rachel Adams";
-	$scope.localStorageVal = localStorage.getItem("localVal");
+	$scope.currentLocalVal = localStorage.getItem("localVal");
+
+
     githubApiService.getRepos().success(function (response) {
-    //Dig into the responde to get the relevant data
-    $scope.drivers = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    	$scope.drivers = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    });
+
     $scope.addDriver = function(){
          	
     }
 
 	$scope.saveToLocalStorage = function(){
 		if(typeof(Storage) !== "undefined") {
-    		localStorage.setItem("localVal", $scope.localStorageVal); // Code for localStorage/sessionStorage.
+    		localStorage.setItem("localVal", $scope.newLocalVal); 
+    		$scope.currentLocalVal = $scope.newLocalVal;
 		} 
 	}
         
-    });
+
 });
