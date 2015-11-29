2a10e6babc2b35f89be32340ba98c28ffb06fdfd
rachelAdams <rachel.adams1012@gmail.com>
2015-11-28 16:43:19 -0800

    lolol strings are hard

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 
-angular.module('rachelApp', [ 'rachelApp.controllers', 'rachelApp.services'])
+angular.module('rachelApp', [ 'rachelApp.controllers', 'rachelApp.services']);
+
+
+
+angular.module('rachelApp');
 .directive('mySharedScope', function () {
     return {
-        template: "Name: <span ng-bind="driver.Driver.givenName"></span> <span ng-bind="driver.Driver.familyName"></span><br /> Wikipedia: <span ng-bind="driver.Driver.url"></span>"
+        template: "Name: <span ng-bind='driver.Driver.givenName'></span> <span ng-bind='driver.Driver.familyName'></span><br /> Wikipedia: <span ng-bind='driver.Driver.url'></span>"
     };
 });
