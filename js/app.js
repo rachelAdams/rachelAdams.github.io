6b4a384ea28ab57dad6663315927c31258b1b644
rachelAdams <rachel.adams1012@gmail.com>
2015-11-28 16:33:53 -0800

    made a directive!

--- js/app.js
+++ js/app.js
@@ -2,4 +2,10 @@
 angular.module('rachelApp', [
   'rachelApp.controllers',
   'rachelApp.services'
-]);
+]).directive('mySharedScope', function () {
+    return {
+        template: 'Name: <span ng-bind="driver.Driver.givenName"></span> 
+        <span ng-bind="driver.Driver.familyName"></span><br />
+        Wikipedia: <span ng-bind="driver.Driver.url"></span>'
+    };
+});;
